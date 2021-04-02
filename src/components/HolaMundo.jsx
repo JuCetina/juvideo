eca72e4c0f09610bd08629bc8de3d697454b979d
jucetina <juliethcetina@gmail.com>
2021-04-01 22:17:44 -0500

    Primer commit

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../assets/styles/App.scss';
+
+const HolaMundo = () => (
+    <h1>Hola Mundo!!!!!</h1>
+);
+
+export default HolaMundo;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HolaMundo from './components/HolaMundo';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<HolaMundo />, app);
