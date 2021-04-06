60047b3138bf966780cfff29ea408a33fec9bb68
jucetina <juliethcetina@gmail.com>
2021-04-06 17:37:50 -0500

    Commit final

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -5,16 +5,16 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ title, image, year, rating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260" alt=""  />
+        <img className="carousel-item__img" src={ image } alt={`Cubierta de ${ title }`}  />
         <div className="carousel-item__details">
             <div>
                 <img className="carousel-item__details--img" src={ playIcon } alt="Play Icon" /> 
                 <img className="carousel-item__details--img" src={ plusIcon } alt="Plus Icon" /> 
             </div>
-            <p className="carousel-item__details--title">Título descriptivo</p>
-            <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+            <p className="carousel-item__details--title">{ title }</p>
+            <p className="carousel-item__details--subtitle">{`${ year } - Calificación: ${ rating }/10 - ${ duration } minutos`}</p>
         </div>
     </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, ueEffect, useEffect } from 'react';
 
 import '../assets/styles/App.scss';
 
@@ -9,19 +9,55 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
-const App = () => (
-    <>
-        <Header />
-        <Search />
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </>
-);
+import useInitialState from '../hooks/useInitialState';
+
+const SCI_FI_API = 'https://yts.mx/api/v2/list_movies.json?genre=sci-fi';
+const ANIMATION_API = 'https://yts.mx/api/v2/list_movies.json?genre=animation';
+const DRAMA_API = 'https://yts.mx/api/v2/list_movies.json?genre=drama';
+
+const App = () => {
+
+    const fiction = useInitialState(SCI_FI_API);
+    const animation = useInitialState(ANIMATION_API);
+    const drama = useInitialState(DRAMA_API);
+
+    return (
+        <>
+            <Header />
+            <Search />
+            
+            {fiction.data.movies.length > 0 && 
+            
+                <Categories title="Ciencia Ficción">
+                    <Carousel>
+                        {fiction.data.movies.map(item => 
+
+                            <CarouselItem key={item.id} title={item.title} image={item.medium_cover_image} year={item.year} duration={item.runtime} rating={item.rating} />
+                        )}
+                    </Carousel>
+                </Categories> 
+            }
+
+            <Categories title="Animación">
+                <Carousel>
+                    {animation.data.movies.map(item => 
+
+                        <CarouselItem key={item.id} title={item.title} image={item.medium_cover_image} year={item.year} duration={item.runtime} rating={item.rating} />
+                    )}
+                </Carousel>
+            </Categories>
+
+            <Categories title="Drama">
+                <Carousel>
+                    {drama.data.movies.map(item => 
+
+                        <CarouselItem key={item.id} title={item.title} image={item.medium_cover_image} year={item.year} duration={item.runtime} rating={item.rating} />
+                    )}
+                </Carousel>
+            </Categories>
+            <Footer />
+        </>
+    );
+}
 
 export default App;
